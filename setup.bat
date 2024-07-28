14a130300a90dfd6c01a65c92f49fbff51afb9c4
Ernan Hughes <155574808+ernanhughes@users.noreply.github.com>
2024-07-28 08:37:03 +0100

    kaggle notebooks

--- setup.bat
+++ setup.bat
@@ -2,4 +2,4 @@ python -m evn .env
 python.exe -m pip install --upgrade pip
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install -r .\requirements.txt
-.\.venv\Scripts\activate         
+start ".\.venv\Scripts\activate"         
